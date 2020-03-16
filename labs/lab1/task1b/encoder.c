6072aee58cce03a76ca174668f44f12c15880f97
yoav.keissar <yoav.keissar@servicenow.com>
2020-03-16 18:41:10 +0200

    1b v1

--- labs/lab1/task1b/encoder.c
+++ labs/lab1/task1b/encoder.c
@@ -1,13 +1,19 @@
 #include<stdio.h> 
 
 int main(int argc,char* argv[]){
-    int c;
+    bool isDebug = flase;
+    if(argc > 1 && argv[1] == '-D')
+        isDebug = true;
+    int org, mod, i=0;
+    char[] str = fgets(stdin);
     do{
-
-        c = fgetc(stdin);
-        if(c>=97 && c<=122)
-            c-= 32;
-        printf("%c",c);
-    } while(c != EOF);
+        org = str[i];
+        mod = org;
+        if(org >= 97 && org <= 122){
+            mod = org - 32;
+        }
+        printf("%d\t%d\n",org,mod);
+        i++;
+    } while(corg != EOF);
 	return 0;
 }
