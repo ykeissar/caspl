a266fbfe2a988ec9c3fcf2fb09ad1201236a07d7
caspl202 <ykeissar131@gmail.com>
2020-03-17 21:19:03 +0200

    task1c final

--- labs/lab1/task1c/encoder.c
+++ labs/lab1/task1c/encoder.c
@@ -1,32 +1,48 @@
 #include<stdio.h> 
+#include <string.h>
 
 int main(int argc,char* argv[]){
     int isAdd = 0, recieveKey = 0;
-    if(argc > 1)
+    int len=1;
+    if(argc > 1){
         if(argv[1][0] == '+'){
             isAdd = 1;
+        }
         recieveKey = 1;
+        len = strlen(argv[1])-2;
     }
-    int org, mod, i = 2;
+    
+    int org, mod, i = 0;
+    int dif;
+
     do{
         org = getc(stdin);
         mod = org;
-        if(!recieveKey)
+        if(recieveKey == 0){
             if(org >= 97 && org <= 122)
                 mod = org - 32;
+        }
         else{ 
+            dif = argv[1][i+2];
+            dif -= 48;
             if(isAdd){
-                mod = org + argv[1][i];
-                mod = mod % 255; 
+                mod = org + dif;
+                mod %= 255; 
             }
             else{
-                mod = org - argv[1][i];
+                mod = org - dif;
                 if(mod < 0)
                     mod += 255;  
             }
         }
-        
-        printf("%c",mod);
+        i++;
+        i = i % len;
+        if(org != 10 && org != -1){
+            printf("%c",mod);
+        }
+        else{
+            printf("%c",org);
+        }
     } while(org != EOF);
 	return 0;
 }
