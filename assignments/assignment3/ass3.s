ef8016823ee2c02d05d3e849d9a413210d7499ef
ykeissar <ykeissar131@gmail.com>
2020-06-16 21:12:49 +0300

    ass3: 12312

--- assignments/assignment3/ass3.s
+++ assignments/assignment3/ass3.s
@@ -8,13 +8,26 @@
     popad
 %endmacro
 
-%macro setRand 1
+%macro setRand 2
     pushfd
     pushad
+    push dword %2
     call generateRand
-    mov [ebx+%1],eax
+    add esp,4
+    finit
+    fld qword[numGenerated]
+    fstp qword [ebx+%1]
+    ffree
     popad
     popfd
+%endmacro
+
+%macro pushFloat 1
+    finit
+    fld qword[%1]
+    sub esp,8
+    fstp qword [esp]
+    ffree
 %endmacro
 
 %macro fPushInt 1
@@ -38,13 +51,14 @@
 %endmacro
 
 section .data
-    
+
 section .bss
     N: resb 4
     R: resb 4
     K: resb 4
     d: rest 1
     seed: resb 2
+    numGenerated: resd 2
 
     CORS: resd 1                            ; co-routines aray
     CURR: resd 1                            ; current co-routine
@@ -53,27 +67,31 @@ section .bss
 
 section .rodata
     format_int: db "%d",0
-    format_float: db "%f",0
+    format_float: db "%f",10,0
     format_short: db "%hd",0
     formatDebugNum: db "Generated the number: %f",10,0 
     formatDebugNumSeed: db "Next Seed: %hd",10,0 
+    format_printCor: db "%d, %f, %f, %f, %f, %d",10,0
 
     CODEP equ 0                             ; co-routine struct
     SPP equ 4
     COID equ 8
     CORD_X equ 14
-    CORD_Y equ 18
-    DIRECT equ 22
-    SPEED equ 26
-    SCORE equ 30                            ;total co-routine size - 34
+    CORD_Y equ 22
+    DIRECT equ 30
+    SPEED equ 38
+    SCORE equ 46                          ;total co-routine size - 50 
 
-    COR_SIZE equ 34
+    COR_SIZE equ 50
     STK_SIZE equ 16*1024                     ;16 Kb
 
     WRAPING_ANG: dd 360
     WRAPING_BOARD: dd 100
-    MAX_INT16: dw 0xffff
+    MAX_SPEED: dd 100
+    MAX_INT16: dw 0x7fff
 
+    TARGET_X: dd 8
+    TARGET_Y: dd 8
 
 section .text
     global main
@@ -85,56 +103,44 @@ section .text
     extern free
 
 
-    main:                   ;tests
+    main:
         push ebp
         mov ebp,esp
+        sub esp,4
 
-        mov edx,0
         mov ebx, dword[ebp+12]
+
         add ebx,4
-        mov ecx,dword[ebx]
+        mov edx,[ebx]
+        handleArg edx,format_short,seed
 
-        handleArg ecx,format_short,seed
+        pushad
+        push COR_SIZE
+        call malloc
+        mov [CORS],eax
+        add esp,4
+        popad
 
-        mov ecx,0
-        mov ecx,5
+        pushad
+        push dword 0
+        call initCoState
+        add esp,4
+        popad
 
-        startTestLoop:
-            pushad
-            mov ecx, 0
-            mov cx, word[seed] 
-            push ecx
-            push dword formatDebugNumSeed
-            call printf
-            add esp, 8
-            popad
+        pushad
+        push dword[CORS]
+        call printCor
+        add esp,4
+        popad
 
-            
-            pushad
-            mov ecx, 0
-            mov cx, [MAX_INT16]
-            push ecx
-            call generateRand
-            mov dword[ebp-4],eax
-            add esp,4
-            popad
+        mov ebx,[CORS]
+        call do_resume
 
-            pushad
-            push dword[ebp-4]
-            push dword formatDebugNum
-            call printf
-            add esp, 8
-            popad
-            
-            sub ecx,1
-            cmp ecx,0
-            jnz startTestLoop
-        
         mov esp,ebp
         pop ebp
-        ret
-
-    normal_main:
+        ret 
+         
+    other_main:
         push ebp
         mov ebp,esp
         sub esp,4
@@ -190,26 +196,34 @@ section .text
                 inc ecx
             endInitCorsLoop:
 
+        InitTaget:
+            setRand TARGET_X,100
+            setRand TARGET_Y,100
+
         mov esp,ebp
         pop ebp
         ret
+
 
     initCoState:
         push ebp
         mov ebp,esp
 
         mov ebx,[ebp+8]                         ; cor id
-        mov eax,[COR_SIZE]                      ;
+        mov eax,COR_SIZE                      ;
         mul ebx
-        mov ebx, [eax + CORS]                   ; ebx = COi struct pointer in CORS  //verify [] 
+        mov ebx,[CORS]
+        add eax,ebx
+        mov ebx,eax    ; ebx = COi struct pointer in CORS  //verify [] 
         
-        mov dword [ebx+CODEP],droneFunc 
+        add ebx,CODEP
+        mov dword[ebx],droneFunc 
         
         pushad
-        push dword[STK_SIZE]
+        push dword STK_SIZE
         call malloc                             ; allocate stack to coroutine
         add esp,4
-        add eax,[STK_SIZE]
+        add eax,STK_SIZE
         mov [ebx+SPP],eax
         popad
 
@@ -224,135 +238,158 @@ section .text
         mov eax,[ebp+8]
         mov [ebx+COID],eax
 
-        setRand CORD_X
-        setRand CORD_Y
-        setRand DIRECT
-        setRand SPEED
+        setRand CORD_X,100
+        setRand CORD_Y,100
+        setRand DIRECT,120
+        setRand SPEED,100
 
         mov [ebx+SCORE], dword 0  
         mov esp,ebp
         pop ebp
         ret
 
-    droneFunc:          ;[ebp-8] = rawAngle, [ebp-12]= rawSpeed
-        sub esp,8
+    droneFunc:          ;[ebp-8] = rawAngle, [ebp-16]= rawSpeed
+        sub esp,16          
         finit
         mov ebx,[CURR]                          ;
 
         generateNewFields:
-        generateAngle:
-            pushad  
-            push dword 120             ;120 = upper bound = 100
-            call generateRand        
-            mov dword[ebp-4],eax       ; generate d-angle [0,120]
-            add esp,4
-            popad
+            generateAngle:
+                pushad  
+                push dword 120             ;120 = upper bound = 100
+                call generateRand               ; generate d-angle [0,120]
+                add esp,4
+                popad
 
-            fld dword[ebp-4]         ; set to [-60,60]
-            fPushInt 60
-            fsubp
-            fstp dword[ebp-4]
-            fld dword[ebx+DIRECT]       
-            fld dword[ebp-4]
-            faddp
-            fstp dword[ebp-8]
+                fld qword[numGenerated]         ; set to [-60,60]
+                fPushInt 60
+                fsubp
+                fld qword[ebx+DIRECT]       
+                faddp
+                fstp qword[ebp-8]
+                
+            generateSpeed: 
+                pushad 
+                push dword 20             
+                call generateRand        
+                add esp,4
+                popad
+
+
+                fld qword[numGenerated] 
+                fPushInt 10
+                fsubp                        ; set to [-10,10]
+
+                fld qword[ebx+SPEED]         ;SPEED=offset to relevant field
+                faddp
+                fstp qword[ebp-16]
+                ffree
+            DroneMovement:                   ;(x1,y1) -> (x1+v*cosa, y1+v*sina)
+                finit
+                computingX:
+                    fld qword[ebx+DIRECT]           ;transfering angke to radians representation
+                    fldpi
+                    fmulp
+                    fPushInt 180
+                    fdivp
+                    fcos                            ; result not necceserly in 32 bits 
+                    fld qword[ebx+SPEED]
+                    fmulp 
+                    fld qword[ebx+CORD_X]
+                    faddp
+                    
+                    fst qword[ebx+CORD_X]
+
+                    fPushInt [WRAPING_BOARD]                    ; check if pass limit
+                    fcomp
+                    jae notAbove100X                     ;jumps if st(0)<=st(1)
+                        fPushInt [WRAPING_BOARD] 
+                        fsubp 
+                    notAbove100X:
+                    fPushInt 0
+                    fcomp 
+                    jae notBelowX0
+                        fPushInt [WRAPING_BOARD]
+                        faddp
+                    notBelowX0:
+                    fstp qword[ebx+CORD_X]
+                computingY:             
+                    fld qword[ebx+DIRECT]           ;transfering angke to radians representation
+                    fldpi
+                    fmulp
+                    fPushInt 180
+                    fdivp
+                    fsin                            ; result not necceserly in 32 bits 
+                    fld qword[ebx+SPEED]
+                    fmulp 
+                    fld qword[ebx+CORD_Y]
+                    faddp
+                    
+                    fst qword[ebx+CORD_Y]
+
+                    fPushInt [WRAPING_BOARD]                    ; check if pass limit
+                    fcomp
+                    jae notAbove100Y                     ;jumps if st(0)<=st(1)
+                        fPushInt [WRAPING_BOARD] 
+                        fsubp 
+                    notAbove100Y:
+                    fPushInt 0
+                    fcomp 
+                    jae notBelowY0
+                        fPushInt [WRAPING_BOARD]
+                        faddp
+                    notBelowY0:
+                    fstp qword[ebx+CORD_Y]
+         
+            setNewAngle:
+                fld qword [ebp-8]                   ;load raw new angle
+                fPushInt dword[WRAPING_ANG] 
+                fcomp
+                jae notGreaterAngle
+                    fPushInt dword[WRAPING_ANG]
+                    fsubp
+                notGreaterAngle:
+                fPushInt dword 0
+                fcomp
+                jbe notNegAngle
+                    fPushInt dword[WRAPING_ANG]
+                    faddp
+                notNegAngle:
+                fstp qword [ebx+DIRECT]
+
+            setNewSpeed:
+                fld qword [ebp-16]                   ;load raw new speed
+                fPushInt dword[MAX_SPEED] 
+                fcomp
+                jae notGreaterSpeed
+                    fPushInt dword[MAX_SPEED]
+                notGreaterSpeed:
+                fPushInt dword 0
+                fcomp
+                jae notNegSpeed
+                    fPushInt dword 0
+                notNegSpeed:
+                fstp qword [ebx+SPEED]
+
+        MayDestroy:
             
-        generateSpeed: 
-            pushad 
-            push dword 20             
-            call generateRand        
-            mov dword[ebp-4],eax         ; generate d-speed [0,20]
-            add esp,4
-            popad
+            DestroyTarget:
+                add dword[ebx+SCORE],1
 
+                setRand TARGET_X,100
+                setRand TARGET_Y,100
 
-            fld dword[ebp-4]
-            fPushInt 10
-            fsubp                        ; set to [-10,10]
-            fstp dword[ebp-4]
+                jmp generateNewFields
 
-            fld dword[ebx+SPEED]         ;SPEED=offset to relevant field
-            fld dword[ebp-4]
-            faddp
-            fstp dword[ebp-12]
-        
-        DroneMovement:                   ;(x1,y1) -> (x1+v*cosa, y1+v*sina)
-        finit
-        ;computingX
-        fld dword[ebx+DIRECT]           ;transfering angke to radians representation
-        fldpi
-        fmulp
-        fPushInt 180
-        fdivp
-        fcos                            ; result not necceserly in 32 bits 
-        fld dword[ebx+SPEED]
-        fmulp 
-        fld dword[ebx+CORD_X]
-        faddp
-        
-        fst dword[ebx+CORD_X]           ;chek if abov range
-        fPushInt 100
-        fcom
-        jbe notAbove100X                     ;jumps if st(0)<=st(1)
-        fsubp 
-        notAbove100X:
-            fld dword[ebx+CORD_X]
-            fPushInt 0
-            fcomp 
-            jae notBelowX0
-            fPushInt 100
-            faddp
-        notBelowX0:
-            fstp dword[ebx+CORD_X]
-        ;computingY              
-        fld dword[ebx+DIRECT]           ;transfering angke to radians representation
-        fldpi
-        fmulp
-        fPushInt 180
-        fdivp
-        fcos                            ; result not necceserly in 32 bits 
-        fld dword[ebx+SPEED]
-        fmulp 
-        fld dword[ebx+CORD_Y]
-        faddp
-        
-        fst dword[ebx+CORD_Y]           ;chek if abov range
-        fPushInt 100
-        fsin
-        jbe notAbove100Y                     ;jumps if st(0)<=st(1)
-        fsubp 
-        notAbove100Y:
-            fld dword[ebx+CORD_Y]
-            fPushInt 0
-            fcomp 
-            jae notBelowY0
-            fPushInt 100
-            faddp
-        notBelowY0:
-            fstp dword[ebx+CORD_Y]
-        ffree
-        add esp,8
-        cmp dword[ebp-8], 360              ;verify works
-        jna notGreaterAngle  
-            fld dword [WRAPING_ANG]
-            fld dword [ebp-8]
-            fsubp
-            fstp dword [ebx+DIRECT]
-        notGreaterAngle:
-        
-        cmp dword[ebp-8],0       ;lower bound=0                           ;verify works
-        jna notLessAngle 
-            fld dword [WRAPING_ANG]
-            fld dword [ebp-8]
-            faddp
-            fstp dword [ebx+DIRECT]   
-        notLessAngle:
-
-        generateAndSetCords:
-        
-        
-        ffree
+        pushad
+        push dword[CORS]
+        call printCor
         add esp,4
+        popad
+        
+        
+        ffree
+        add esp,16
 
     generateRand:                         ;operand: upper bound ; generate num between 0-X
         push ebp
@@ -394,10 +431,43 @@ section .text
         fdivp
         fild word[ebp+8]                ;CHECK if theres a conflict between input-usingfunc
         fmulp
-        fstp dword [ebp-4] 
+        fabs
+        fstp qword [numGenerated] 
         ffree
-        mov eax, [ebp-4]
 
         mov esp,ebp
         pop ebp
         ret
+
+    printCor:
+        push ebp
+        mov ebp,esp
+
+        mov ebx,[ebp+8]             ;ebx=pointer to curr cor         
+        pushad
+
+        push dword[ebx+SCORE]         
+        pushFloat ebx+SPEED
+        pushFloat ebx+DIRECT
+        pushFloat ebx+CORD_Y
+        pushFloat ebx+CORD_X
+        push dword[EBX+COID]
+        push format_printCor
+        call printf
+        
+        add esp,44
+        popad
+        pop ebp
+        ret
+
+    resume: ; save state of current co-routine
+        pushfd
+        pushad
+        mov EDX, [CURR]
+        mov [EDX+SPP], ESP ; save current ESP
+    do_resume: ; load ESP for resumed co-routine
+        mov ESP, [EBX+SPP]
+        mov [CURR], EBX
+        popad ; restore resumed co-routine state
+        popfd
+        ret ; "return" to resumed co-routine
